18625cabe765f61b85b80517cca5d9fe7a14304c
MrLaikz <mrlaikz@gmail.com>
2022-01-20 10:15:44 +0100

    Adding stream cooldown Adding cooldown in config

--- Core-waterfall/src/main/java/fr/iban/bungeecore/commands/StreamCMD.java
+++ Core-waterfall/src/main/java/fr/iban/bungeecore/commands/StreamCMD.java
@@ -9,9 +9,14 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class StreamCMD extends Command {
 
     private final CoreBungeePlugin plugin;
+    private Map<UUID, Long> cooldowns = new HashMap<>();
 
     public StreamCMD(String name, String permission, CoreBungeePlugin plugin) {
         super(name, permission);
@@ -22,17 +27,27 @@ public class StreamCMD extends Command {
     public void execute(CommandSender sender, String[] args) {
         if(sender instanceof ProxiedPlayer) {
             ProxiedPlayer p = (ProxiedPlayer) sender;
-            if(args.length == 0 || args.length > 1) {
+            if(args.length != 1) {
                 p.sendMessage("§cSyntaxe: /stream <link>");
             }
 
             if(args.length == 1) {
+
+                if(!cooldowns.isEmpty() && cooldowns.containsKey(p.getUniqueId())) {
+                    int secondsLeft = (int) (Integer.parseInt(plugin.getConfiguration().get("cooldowns.stream").toString()) * 60 * 1000 - (System.currentTimeMillis() - cooldowns.get(p.getUniqueId()))) / 1000;
+                    if(secondsLeft <= 0) {
+                        cooldowns.remove(p.getUniqueId());
+                    }else if(!p.hasPermission("spartacube.stream.bypasscooldown")){
+                        p.sendMessage("§cVous devez attendre " + secondsLeft +" secondes avant de réutiliser cette commande !");
+                        return;
+                    }
+                }
+
                 String link = args[0];
-                p.sendMessage("§aAnnonce effectuée");
-                for(ProxiedPlayer pl : ProxyServer.getInstance().getPlayers()) {
+                for (ProxiedPlayer pl : ProxyServer.getInstance().getPlayers()) {
                     AccountProvider ap = new AccountProvider(pl.getUniqueId());
                     Account account = ap.getAccount();
-                    if(account.getOption(Option.STREAM)) {
+                    if (account.getOption(Option.STREAM)) {
                         pl.sendMessage("§e§l          ≫ Annonce Live ≪  ");
                         pl.sendMessage("");
                         pl.sendMessage("§7§l► §5§l" + p.getName() + "§f§lVient de lancer son live !");
@@ -40,6 +55,10 @@ public class StreamCMD extends Command {
                         pl.sendMessage("§7§l► §c§lLien : §f§e" + link);
                         pl.sendMessage("");
                     }
+                }
+
+                if(!p.hasPermission("spartacube.stream.bypasscooldown")){
+                    cooldowns.put(p.getUniqueId(), System.currentTimeMillis());
                 }
             }
         }
