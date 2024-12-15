a22c7702b58aa4b551d02c992e3dbf09b0676197
Iban <iban.etchepareborde@gmail.com>
2024-12-15 15:02:26 +0100

    plain text serializer

--- Core-paper/src/main/java/fr/iban/bukkitcore/utils/ChatUtils.java
+++ Core-paper/src/main/java/fr/iban/bukkitcore/utils/ChatUtils.java
@@ -2,6 +2,7 @@ package fr.iban.bukkitcore.utils;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
@@ -14,5 +15,9 @@ public class ChatUtils {
 
     public static Component translateColors(String string) {
         return LegacyComponentSerializer.legacyAmpersand().deserialize(string);
+    }
+
+    public static String toPlainText(Component component) {
+        return PlainTextComponentSerializer.plainText().serialize(component);
     }
 }
